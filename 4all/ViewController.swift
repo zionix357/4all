4113f688dfc3b46523280d52f13f93ded81609ce
Arthur Rocha <arthurrocha1357@gmail.com>
2019-07-22 18:48:00 -0300

    Initial project

---
+++ 4all/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  4all
+//
+//  Created by Arthur Rocha on 22/07/19.
+//  Copyright © 2019 Arthur Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
